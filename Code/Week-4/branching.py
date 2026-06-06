98468f763c59d41180f4b663d9aaaacc1d6b5ec1
Ashwin Prasanth <118043272+AshwinPrasanth@users.noreply.github.com>
2026-06-06 18:36:39 +0100

    Implement MostFractionalBranching strategy class

---
+++ Code/Week-4/branching.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class BranchingStrategy(ABC):
+    """
+    Base interface for branching rules.
+    """
+
+    @abstractmethod
+    def select(self, lp_solution: np.ndarray) -> int:
+        pass
+
+
+class MostFractionalBranching(BranchingStrategy):
+    """
+    Classical branching rule.
+
+    Choose the variable closest to 0.5.
+    """
+
+    def select(self, lp_solution: np.ndarray) -> int:
+
+        fractional_vars = []
+
+        for i, value in enumerate(lp_solution):
+
+            if 1e-6 < value < 1 - 1e-6:
+                fractional_vars.append(i)
+
+        if not fractional_vars:
+            return -1
+
+        return min(
+            fractional_vars,
+            key=lambda i: abs(lp_solution[i] - 0.5)
+        )
+        
+import numpy as np
+
+#from branching import MostFractionalBranching
+
+x = np.array([
+    1.0,
+    0.3,
+    0.52,
+    0.9,
+    0.48,
+])
+
+brancher = MostFractionalBranching()
+
+print(brancher.select(x))
